62cec02bbe21b819841084482bfaaf87bf8e0ac2
anasrul03 <anasrul03@gmail.com>
2023-04-04 00:07:51 +0800

    push night not settled websocket

--- chat.js
+++ chat.js
@@ -1,21 +1,19 @@
-const ws = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket("ws://localhost:8080");
+// let userMsg = [];
 
 // const printMsg = () =
 
-ws.addEventListener("open", () => {
-  ws.send("New user joined the chat");
+socket.addEventListener("open", () => {
+  socket.send("New user joined the chat");
+  console.log("entry here");
 });
 
-ws.addEventListener("message", (event) => {
+socket.addEventListener("message", (event) => {
   const message = event.data;
-  // const divElement = document.createElement("<li>");
-  // const getSpace = document.getElementById("chat-window");
-  // document.getElementById("chat-window").innerHTML = divElement;
-  // divElement.innerText = message;
-  // console.log(message);
+  // socket.send();
 });
 
-ws.addEventListener("close", () => {
+socket.addEventListener("close", () => {
   // update UI to indicate user has left the chat
 });
 
@@ -24,10 +22,38 @@ const messageInput = document.getElementById("message-input");
 
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const chatWindow = document.getElementById("chat-window");
-  const message = messageInput.value;
-  ws.send(message);
+  if (!socket) {
+    console.log("no socket");
+  }
+  const chatDisplay = document.getElementById("chat-window");
+  const userInput = messageInput.value;
+  const listElement = document.createElement("li");
+  // var valueElement = document.createElement("span");
+  // const time = Date.now();
+  // const changeTime = time.toLocaleDateString();
+  // const str = new Date();
+  // const datetime = str.today() + str.timeNow();
+  // const [dateComponents, timeComponents] = datetime.split(" ");
+  // const [month, day, year] = dateComponents.split("/");
+  // const [hours, minutes, seconds] = timeComponents.split(":");
+  // const date = new Date(+year, month - 1, +day, +hours, +minutes, +seconds);
+
+  listElement.innerText = `${userInput} []`;
+
+  chatDisplay.appendChild(listElement);
+
+  // console.log(date);
+
+  // divElemnt.appendChild(valueElement);
+  // valueElement.innerText = "message";
+  // chatWindow.innerHTML = `<p>${message}</p>`;
+  // divElement.innerHTML = `<p>${message}</p>`;
+  // userMsg.push(message);
+
+  socket.send(messageInput.value);
+  // console.log(messageInput.value);
+
+  console.log(userInput);
+
   messageInput.value = "";
-  console.log(message);
-  chatWindow.innerHTML = `<p>${message}</p>`;
 });
